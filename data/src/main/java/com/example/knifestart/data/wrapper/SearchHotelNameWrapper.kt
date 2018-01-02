de9e50614ea94b090b69274f6db15cdb72864b44
GlebKalinichenko <gleb.kalinichenko@umbrella-web.com>
2018-01-02 23:15:16 +0300

    Implement test for converters from response data to local data

--- data/src/main/java/com/example/knifestart/data/wrapper/SearchHotelNameWrapper.kt
+++ data/src/main/java/com/example/knifestart/data/wrapper/SearchHotelNameWrapper.kt
@@ -1,5 +1,7 @@
 package com.example.knifestart.data.wrapper
 
+import com.example.knifestart.domain.entity.Hotel
+import com.example.knifestart.domain.response.HotelResponse
 import javax.inject.Inject
 
 /**
@@ -7,4 +9,18 @@ import javax.inject.Inject
  */
 class SearchHotelNameWrapper @Inject constructor() {
 
+    fun convertHotelReponse(hotelResponse: HotelResponse) : Hotel {
+        var hotel = Hotel(hotelResponse.name)
+        return hotel
+    }
+
+    fun convertHotelReponse(hotelResponses: List<HotelResponse>) : MutableList<Hotel> {
+        var hotels = mutableListOf<Hotel>()
+
+        for (hotelResponse in hotelResponses) {
+            hotels.add(Hotel(hotelResponse.name))
+        }
+
+        return hotels
+    }
 }
---
+++ data/src/test/java/com/example/knifestart/data/wrapper/SearchHotelWrapperTest.kt
@@ -0,0 +1,43 @@
+package com.example.knifestart.data.wrapper
+
+import com.example.knifestart.domain.entity.Hotel
+import com.example.knifestart.domain.response.HotelResponse
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+
+/**
+ * Created by glebkalinichenko on 02.01.18.
+ */
+class SearchHotelWrapperTest {
+    var wrapper = SearchHotelNameWrapper()
+
+    @Test
+    fun testTransformHotelResponse() {
+        var response = HotelResponse("Intourist")
+        var hotel = wrapper.convertHotelReponse(response)
+
+        assertThat(hotel, `is`(instanceOf(Hotel::class.java)))
+        assertThat(hotel.name, `is`("Intourist"))
+    }
+
+    @Test
+    fun testTransformHotelListResponse() {
+        var mockHotelResponseOne = mock(HotelResponse::class.java)
+        var mockHotelResponseTwo = mock(HotelResponse::class.java)
+
+        `when`(mockHotelResponseOne.name).thenReturn("Intourist")
+        `when`(mockHotelResponseTwo.name).thenReturn("Intourist")
+
+        var listResponses = listOf<HotelResponse>(mockHotelResponseOne, mockHotelResponseTwo)
+        var listHotels = wrapper.convertHotelReponse(listResponses)
+
+        assertThat(listHotels[0], `is`(instanceOf(Hotel::class.java)))
+        assertThat(listHotels[1], `is`(instanceOf(Hotel::class.java)))
+        assertThat(listHotels.size, `is`(2))
+    }
+}
